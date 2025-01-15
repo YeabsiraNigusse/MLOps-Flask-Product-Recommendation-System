692a9168371a9f65ea6325dc395a53bca3f5378d
YeabsiraNigusse <nigusseyeabsira@gmail.com>
2025-01-15 17:56:05 +0300

    added features to implement

--- app.py
+++ app.py
@@ -28,3 +28,19 @@ def recommend():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+
+# features i want to add on this application
+# 1. add sign up functionality to add new user. basic details should be required
+# 2. add login functionality to login to the application
+# 3. welcome the user login by getting the user name from the database and show him dashboard
+# 4. the dashboard simply show list of some product recommendation based on the user id
+# 5. it would be cool if the user sign up and login have email varificationa and password reset functionality and also login with google.
+
+
+
+# As a fix though instead of pridicting the rating by taking the user id and product id, 
+# i want product pridiction based on user information
+# so when i user logs in with some information the model will pridict products based on that user information.
+# so the model take user id and pridict the product rating for each product found in the database 
+# so i can sort them and show the top 10 products to the user.
